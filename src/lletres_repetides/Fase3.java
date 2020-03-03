91abc043c18c22ff2248ea64c47ab008136c3497
garciacarlos78 <51118101+garciacarlos78@users.noreply.github.com>
2020-03-03 13:14:56 +0100

    Added outputs to phases 3 and 4

--- src/lletres_repetides/Fase3.java
+++ src/lletres_repetides/Fase3.java
@@ -9,13 +9,13 @@ public class Fase3 {
 
 	public static void main(String[] args) {
 
-		List<Character> name = Arrays.asList('C', 'a', 'r', 'l', 'o', 's');
+		List<Character> name = Arrays.asList('C', 'a', 'r', 'l', 'o', 's', 's');
 
 		Map<Character, Integer> nameMap = new HashMap();
 
 		for (Character c : name) {
 
-			// La lletra és vocal
+			// La lletra ï¿½s vocal
 			if ( c=='a' || c=='e' || c=='i' || c=='o' || c=='u' ||
 					c=='A' || c=='E' || c=='I' || c=='O' || c=='U') {
 				// print vowel message
@@ -25,13 +25,13 @@ public class Fase3 {
 				if (nameMap.get(c) == null) nameMap.put(c,  1);
 				else nameMap.replace(c, nameMap.get(c)+1);
 
-			// És número
+			// ï¿½s nï¿½mero
 			} else if ( c=='0' || c=='1' || c=='2' || c=='3' || c=='4' ||
 					c=='5' || c=='6' || c=='7' || c=='8' || c=='9') {
 				// print number message
-				// System.out.println("Els noms de persones no contenen números!");
+				// System.out.println("Els noms de persones no contenen nÃºmeros!");
 
-			// La lletra és consonant
+			// La lletra ï¿½s consonant
 			} else {
 				// print consonant message
 				// System.out.println("CONSONTANT");
@@ -41,5 +41,8 @@ public class Fase3 {
 				else nameMap.replace(c, nameMap.get(c)+1);
 			}
 		}
+		
+		System.out.println("Nom introduÃ¯t: " + name.toString());
+		System.out.println("Contingut del mapa: " + nameMap.toString());
 	}
 }
--- src/lletres_repetides/Fase4.java
+++ src/lletres_repetides/Fase4.java
@@ -16,7 +16,7 @@ public class Fase4 {
 
 		for (Character c : name) {
 
-			// La lletra és vocal
+			// La lletra ï¿½s vocal
 			if ( c=='a' || c=='e' || c=='i' || c=='o' || c=='u' ||
 					c=='A' || c=='E' || c=='I' || c=='O' || c=='U') {
 				// print vowel message
@@ -26,13 +26,13 @@ public class Fase4 {
 				if (nameMap.get(c) == null) nameMap.put(c,  1);
 				else nameMap.replace(c, nameMap.get(c)+1);
 
-			// És número
+			// ï¿½s nï¿½mero
 			} else if ( c=='0' || c=='1' || c=='2' || c=='3' || c=='4' ||
 					c=='5' || c=='6' || c=='7' || c=='8' || c=='9') {
 				// print number message
-				// System.out.println("Els noms de persones no contenen números!");
+				// System.out.println("Els noms de persones no contenen nï¿½meros!");
 
-			// La lletra és consonant
+			// La lletra ï¿½s consonant
 			} else {
 				// print consonant message
 				// System.out.println("CONSONTANT");
@@ -43,12 +43,16 @@ public class Fase4 {
 			}
 		}
 		
-		List<Character> surname = Arrays.asList('G','a','r','c','í','a');
+		List<Character> surname = Arrays.asList('G','a','r','c','Ã­','a');
 		
 		List<Character> fullName = new ArrayList<Character>(name);
 		
 		fullName.add(' ');
 		
 		fullName.addAll(surname);
+		
+		System.out.println("Variable name: " + name.toString());
+		System.out.println("Variable surname: " + surname.toString());
+		System.out.println("Variable fullName: " + fullName.toString());
 	}
 }
